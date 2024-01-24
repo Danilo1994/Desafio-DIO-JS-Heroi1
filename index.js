c44ef16d50383675f21c1ba49ad623ef227d268a
Danilo1994 <danilovilasboaslima@gmail.com>
2024-01-23 22:20:30 -0300

    Estruturas de Decisão

---
+++ index.js
@@ -0,0 +1,37 @@
+//variavel para armazenar XP
+let xp = "10.004"
+
+//estrutura de repetição
+if(xp <= 1.000 )
+{
+    console.log("O Herói Danilo está no nível Ferro ")
+
+}
+else if(xp >= 1.001 && xp <= 2.000)
+{
+    console.log("O Herói Danilo está no nível Bronze ")
+}
+else if(xp >= 2.001 && xp <= 5.000)
+{
+    console.log("O Herói Danilo está no nível Prata ")
+}
+else if( xp >= 5.001 && xp <= 7.000)
+{
+    console.log("O Herói Danilo está no nível Ouro")
+}
+else if( xp >= 7.001 && xp <= 8.000)
+{
+    console.log("O Herói Danilo está no nível Platina")
+}
+else if( xp >= 8.001 && xp <= 9.000)
+{
+    console.log("O Herói Danilo está no nível Ascendente")
+}
+else if( xp >= 9.001 && xp <= 10.000)
+{
+    console.log("O Herói Danilo está no nível Imortal")
+}
+else if(xp >= 10.001)
+{
+    console.log("O Herói Danilo está no nível radiante")
+}
